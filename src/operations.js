b82ec1ac11ddf01b6e89a6bd9471aad734218357
z5367528 <z5367528@gitlab.cse.unsw.EDU.AU>
2023-06-06 23:58:27 +1000

    Started on basic calculator input functionality

--- src/operations.js
+++ src/operations.js
@@ -1,4 +1,29 @@
-getElemen
+const one = document.getElementById("1");
+const two = document.getElementById("2");
+const three = document.getElementById("3");
+const four = document.getElementById("4");
+const five = document.getElementById("5");
+const six = document.getElementById("6");
+const seven = document.getElementById("7");
+const eight = document.getElementById("8");
+const nine = document.getElementById("9");
+
+const calcNumbers = [one, two, three, four, five, six, seven, eight, nine];
+const display = document.getElementById("");
+let displayNumber = 0;
+
+for (num of calcNumbers) {
+    // the button's IDs correlate to their numerical value
+    const value = Number.parseInt(num.id, 10);
+    num.addEventListener('click', function() {
+        if (displayNumber === 0) {
+            displayNumber = value;
+        } else {
+            displayNumber = parseInt(displayNumber.toString() + value);
+        }
+        alert(displayNumber);
+    });
+}
 
 function add(a, b) {
     return a + b;
