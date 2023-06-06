fca876fc1c1651263fc31ab9ea4384fe5b6c0330
z5367528 <z5367528@gitlab.cse.unsw.EDU.AU>
2023-06-06 19:57:04 +1000

    Created basic operations functions

---
+++ src/operations.ts
@@ -0,0 +1,15 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
+function subtract(a: number, b: number) {
+    return a - b;
+}
+
+function multiply(a: number, b: number) {
+    return a * b;
+}
+
+function divide(a: number, b: number) {
+    return a / b;
+}
